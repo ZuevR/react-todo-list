c6f16b72f6a1986194e2b0583978c3ec86fbd3c0
Zuev Roman <ZuevRG@yandex.ru>
2019-10-24 17:43:48 +0300

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import TodoList from './components/Todo-list';
+import SignUp from './components/Sign-up';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <h1 className="text-center display-3 title">Todo List</h1>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/sign-up">Sign Up</Link>
+      </li>
       <div className="row justify-content-center">
-        <TodoList />
+        <Route exact path="/"><TodoList /></Route>
+        <Route path="/sign-up"><SignUp /></Route>
       </div>
     </div>
   );
---
+++ src/components/Sign-up/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class SignUp extends Component {
+  someFunc = () => {
+    console.log('do smthn');
+  };
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
