956ab1ed5ad185acabb4354b180dea0928ab8570
Zuev Roman <ZuevRG@yandex.ru>
2019-10-26 02:21:29 +0300

    Add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import TodoList from './components/Todo-list';
-import SignUp from './components/Sign-up';
+import SignUp from './containers/Sign-up';
 import Header from './components/Header';
-import SignIn from './components/Sign-in';
+import SignIn from './containers/Sign-in';
 import Layout from './hoc/Layout';
 import './App.css';
 
 function App() {
   return (
     <Layout>
-      <div style={{ width: 200, border: '2px solid' }}>Test</div>
+      <Switch>
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/sign-in" component={SignIn} />
+      </Switch>
     </Layout>
   );
 }
